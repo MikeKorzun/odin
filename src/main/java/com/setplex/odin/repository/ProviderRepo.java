51b7c341757bcdcf68a2f4878ea236c97f1f584d
Anton Shtabnoy <antonsh@setplex.com>
2018-06-12 19:04:00 +0300

    Add Provider repository.

---
+++ src/main/java/com/setplex/odin/repository/ProviderRepo.java
@@ -0,0 +1,17 @@
+package com.setplex.odin.repository;
+
+import com.setplex.odin.entity.Provider;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(readOnly = true)
+public interface ProviderRepo extends JpaRepository<Provider, Integer> {
+
+    @Query("SELECT p FROM Provider p WHERE p.id = ?1")
+    Provider findOneById(int id);
+
+    @Query("SELECT p FROM Provider p")
+    List<Provider> findAll();
+}

e8ebdc4a4cef959ff1bc51ca4ea0a5bf70f0deed
Anton Shtabnoy <antonsh@setplex.com>
2018-06-12 18:54:02 +0300

    Add Provider entity.

--- src/main/java/com/setplex/odin/entity/Provider.java
+++ src/main/java/com/setplex/odin/entity/Provider.java
@@ -1,4 +1,25 @@
 package com.setplex.odin.entity;
 
-public class Provider {
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@AttributeOverrides({
+        @AttributeOverride(name = "id", column = @Column(name = "id"))
+})
+@Getter
+@Setter
+public class Provider extends AbstractEntity {
+
+    private int providerId;
+
+    @Column(length = 64, nullable = false)
+    private String address;
+
+    private boolean status;
+
 }
